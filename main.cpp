89ec536d6b0dd862ced26e9e66fc6d0163947a4a
mcrema-coremap <mcrema@coremapmedical.com>
2022-09-19 23:05:13 -0400

    Simple MKL project

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <mkl.h>
+
+using namespace std;
+
+int main()
+{
+	vector<double> a = {68.8915, -52.4488, 0, -79.4537, 88.7629, 0, 58.6154, -35.4198, 0};
+	vector<double> s(3);
+	vector<double> u(9);
+	vector<double> vh(9);
+
+	// Check version of MKL
+	cout << "Checking MKL Version" << endl;
+	MKLVersion Version;
+	mkl_get_version(&Version);
+	cout << "Major version:           " << Version.MajorVersion << endl;
+	cout << "Minor version:           " << Version.MinorVersion << endl;
+	cout << "Update version:          " << Version.UpdateVersion << endl;
+	cout << "Product status:          " << Version.ProductStatus << endl;
+	cout << "Build:                   " << Version.Build << endl;
+	cout << "Platform:                " << Version.Platform << endl;
+	cout << "Processor optimization:  " << Version.Processor << endl;
+	cout << "================================================================" << endl;
+
+	cout << endl << "Running SVD" << endl;
+	lapack_int result = LAPACKE_dgesdd(LAPACK_ROW_MAJOR, 'A', //'A', // if using LAPACKE_dgesvd
+									   static_cast<lapack_int>(3),
+									   static_cast<lapack_int>(3),
+									   a.data(),
+									   static_cast<lapack_int>(3),
+									   s.data(),
+									   u.data(),
+									   static_cast<lapack_int>(3),
+									   vh.data(),
+									   static_cast<lapack_int>(3));
+
+	cout << "Success" << endl;
+	return 0;
+}
